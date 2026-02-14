d240b072ee0b198f2ada420fceab7d4f34325adc
DigDeepDug <elliotbowers2@gmail.com>
2026-02-14 00:34:08 +0000

    add main

---
+++ main.py
@@ -0,0 +1,34 @@
+# have fun!
+
+
+SIZE = 32
+
+display = [[' ' for i in range(SIZE)] for i in range(SIZE)]
+
+
+should_countinue = True
+generation = 1
+
+gen_char = ord('a')
+
+def update(i: int, j: int) -> None:
+
+
+while should_countinue:
+    print(generation)
+    should_countinue = False
+    new_display = display
+    for i in range(SIZE):
+        for j in range(SIZE):
+            cur_char = display[i][j]
+            neighbor_count = 0
+            for neig
+
+            if neighbor_count == 3 
+            if ord(cur_char) >= ord('a') and ord(cur_char) < ord('z'):
+
+            else:
+
+
+    generation += 1
+
